a155708e42934ed9a6eec0ea1c3f98a6d37d5d64
hyeyoungs <sunzero0116@gmail.com>
2023-04-24 13:17:59 +0900

    init : add project skeleton

---
+++ src/main/java/com/shop/eleventh11/Eleventh11Application.java
@@ -0,0 +1,12 @@
+package com.shop.eleventh11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eleventh11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eleventh11Application.class, args);
+	}
+}
---
+++ src/test/java/com/shop/eleventh11/Eleventh11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.eleventh11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eleventh11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
